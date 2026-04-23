0ca8086fc85828266eb6f0a0a06dd7b7531a094f
Houda <slimanihd06@gmail.com>
2026-04-23 18:52:26 +0200

    Clean: suppression Dockerfile + images locales

--- convert_model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-print(f"TensorFlow: {tf.__version__}")
-print(f"Keras: {tf.keras.__version__}")
-
-model = tf.keras.models.load_model("models/best_model.keras")
-
-model.save("model.keras")
-print("✅ Modèle resauvegardé avec succès en model.keras !")
